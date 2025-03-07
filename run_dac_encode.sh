9ecbaefa2b361da4e784ddb31dc333196272281a
Siddharth Saxena <saxena.siddhu@gmail.com>
2025-03-07 11:02:00 +0100

    remove redundant files

--- run_dac_encode.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python3 $(dirname "$0")/dac_encode_cpu.py \
-  --input_dir '/Users/sid/Music/OST/Dark Cloud Soundtrack 8-bit Remix/wav' \
-  --output_dir '/Users/sid/Music/OST/Dark Cloud Soundtrack 8-bit Remix/dac' \
-  --model_bitrate '8kbps' \
-  --n_quantizers 4 \
-  --device 'cpu'
